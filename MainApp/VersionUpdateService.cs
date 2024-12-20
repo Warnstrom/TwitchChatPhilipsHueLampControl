a22525d86d8aa65050237ad0749bdb1889544af6
Warnstrom <anton.warnstrom@gmail.com>
2024-12-20 12:28:04 +0200

    Updated the Updater executable path

--- MainApp/VersionUpdateService.cs
+++ MainApp/VersionUpdateService.cs
@@ -202,7 +202,7 @@ internal class VersionUpdateService(IConfiguration configuration, IJsonFileContr
     private async Task StartUpdaterAndExit(string directoryPath)
     {
         // Configure the updater process based on the operating system
-        string updaterPath = "TwitchChatHueUpdater";
+        string updaterPath = "./Updater/TwitchChatHueUpdater";
         string arguments = "";
         switch (OS)
         {
